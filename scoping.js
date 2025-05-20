ba0401a40dbf449e366a7faa0615ca16ae90b2bd
Vaibhav Dhaygude <139160482+VaibhavDhaygude07@users.noreply.github.com>
2025-05-20 15:26:21 +0530

    [Vaibhav] Scoping

---
+++ scoping.js
@@ -0,0 +1,12 @@
+let a=5; //global scoping
+let b=4; //global scoping
+function calculator(){
+   
+    function add(a,b){
+        let c=10 //local scoping
+        return a+b+c;
+    }
+   
+    console.log (add(a,b));
+}
+calculator(); 
